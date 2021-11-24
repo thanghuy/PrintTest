99929a36c4ec2d7634dc546102f55374a0a9b53f
unknown <huythang0903@gmail.com>
2021-11-25 00:23:25 +0700

    update

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PrintPDF.Model;
+using Wkhtmltopdf.NetCore;
+
+namespace testapp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        readonly IGeneratePdf _generatePdf;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IGeneratePdf generatePdf)
+        {
+            _logger = logger;
+            _generatePdf = generatePdf;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var data = new Test
+            {
+                Text = "This is a test",
+                Number = 123456
+            };
+
+            return await _generatePdf.GetPdf("Views/Test.cshtml", data);
+        }
+    }
+}
---
+++ Model/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrintPDF.Model
+{
+    public class Test
+    {
+        public string Text { get; set; }
+        public int Number { get; set; }
+    }
+}
